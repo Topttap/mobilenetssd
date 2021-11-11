2f6a5fdfa0d587979dd2824012776985538f4855
Topttap <88994691+Topttap@users.noreply.github.com>
2021-11-11 13:28:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -149,8 +149,10 @@ def event_handle(event):
 
     if msgType == "text":
         msg = str(event["message"]["text"])
-        replyObj = TextSendMessage(text=msg)
-        line_bot_api.reply_message(rtoken, replyObj)
+        if (msg == "สวัสดี") :
+            replyObj = TextSendMessage(text=หวัดไม่ดีนะครับ)
+        else :
+            line_bot_api.reply_message(rtoken, replyObj)
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
